b5e007b9a35c566ab9e3fdb2de977bb2d77216cf
BigBobandy <jdykes207@gmail.com>
2024-04-02 18:27:02 -0400

    Refactor Work component to include work experience details

--- my-app/app/components/Work.tsx
+++ my-app/app/components/Work.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="flex flex-col  max-w-md lg:w-1/4  bg-opacity-50 rounded-lg ">
+      <h2 className="text-2xl mb-2 font-medium text-orange-100">
+        Work Experience
+      </h2>
+      <p className="text-md font-light text-white md:mb-4">
+        As a dedicated full-time student expected to graduate in August 2024
+        with a degree in Web Site Design/Development, I am keen on finding
+        opportunities that allow me to apply and expand my burgeoning skills in
+        practical settings.
+      </p>
+      <p className="text-md font-light text-white md:mt-6">
+        While I pursue work opportunities, I am also committed to enhancing my
+        skills through personal projects in my spare time. My ambition is to
+        become a leading full-stack developer, recognized for my ability to
+        tackle complex challenges with innovative solutions.
+      </p>
+    </div>
+  );
 };
 
 export default Work;
