d2e440bbf892fd045329ff15c49fb4f064e97c98
BigBobandy <jdykes207@gmail.com>
2024-04-02 18:27:34 -0400

    Refactor Links component to improve layout and add resume icon

--- my-app/app/components/Links.tsx
+++ my-app/app/components/Links.tsx
@@ -1,47 +1,63 @@
 import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
-import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFilePdf, faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Links = () => {
   return (
-    <div>
-      <h3 className="text-2xl mb-2 font-medium text-orange-100">My Links</h3>
+    <div className="flex flex-col md:gap-1">
+      <h2 className="text-2xl mb-4 font-medium text-orange-100">My Links</h2>
+      <div className="flex flex-row lg:flex-col mb-10 lg:gap-1">
+        <a
+          href="https://www.linkedin.com/in/jackson-dykes-275746240/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="LinkedIn"
+          className="w-full"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className="text-secondary hover:text-tertiary w-12 h-12 lg:w-20 lg:h-20"
+          />
+        </a>
 
-      <a
-        href="https://www.linkedin.com/in/jackson-dykes-275746240/"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="LinkedIn"
-      >
-        <FontAwesomeIcon
-          icon={faLinkedin}
-          className="text-3xl text-secondary hover:text-tertiary"
-        />
-      </a>
+        <a
+          href="https://github.com/BigBobandy"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="GitHub"
+          className="w-full"
+        >
+          <FontAwesomeIcon
+            icon={faSquareGithub}
+            className="text-secondary hover:text-tertiary w-12 h-12 lg:w-20 lg:h-20"
+          />
+        </a>
 
-      <a
-        href="https://github.com/BigBobandy"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="GitHub"
-      >
-        <FontAwesomeIcon
-          icon={faSquareGithub}
-          className="text-3xl  text-secondary hover:text-tertiary"
-        />
-      </a>
-
-      <a
-        href="mailto:jdykesdev@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Email"
-      >
-        <FontAwesomeIcon
-          icon={faSquareEnvelope}
-          className="text-3xl  text-secondary hover:text-tertiary"
-        />
-      </a>
+        <a
+          href="mailto:jdykesdev@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Email"
+          className="w-full"
+        >
+          <FontAwesomeIcon
+            icon={faSquareEnvelope}
+            className="text-secondary hover:text-tertiary w-12 h-12 lg:w-20 lg:h-20"
+          />
+        </a>
+        <a
+          href="/my-resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Resume"
+          className="w-full"
+        >
+          <FontAwesomeIcon
+            icon={faFilePdf}
+            className="text-secondary hover:text-tertiary w-12 h-12 lg:w-20 lg:h-20 lg:ml-2"
+          />
+        </a>
+      </div>
     </div>
   );
 };
