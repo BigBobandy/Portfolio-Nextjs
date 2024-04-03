be30caa2b86cc0efd421f8521617fe7930997d44
BigBobandy <jdykes207@gmail.com>
2024-04-02 20:19:44 -0400

    Update Work component description

--- my-app/app/components/Work.tsx
+++ my-app/app/components/Work.tsx
@@ -8,9 +8,9 @@ const Work = () => {
       </h2>
       <p className="text-md font-light text-white md:mb-4">
         As a dedicated full-time student expected to graduate in August 2024
-        with a degree in Web Site Design/Development, I am keen on finding
-        opportunities that allow me to apply and expand my burgeoning skills in
-        practical settings.
+        with a degree in Web Site Design/Development, I am looking for
+        opportunities that allow me to apply and expand my skills in practical
+        settings.
       </p>
       <p className="text-md font-light text-white md:mt-6">
         While I pursue work opportunities, I am also committed to enhancing my
