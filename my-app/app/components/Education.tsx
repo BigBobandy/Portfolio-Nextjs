30766f188d3fdfcc6bdf7249ae3825d531912dff
BigBobandy <jdykes207@gmail.com>
2024-08-02 16:53:20 -0400

    Update Education component styles and structure

--- my-app/app/components/Education.tsx
+++ my-app/app/components/Education.tsx
@@ -2,33 +2,42 @@ import Image from "next/image";
 
 const Education = () => {
   return (
-    <ul className="md:mb-4 ">
-      <li className="text-2xl mb-2 font-medium text-orange-100">Education</li>
-      <li className="flex flex-col justify-between md:items-center lg:justify-start lg:items-start">
-        <h3 className="text-xl mb-2 font-semibold md:mb-0 md:mr-4">
-          Georgia Northwestern <br /> Technical College
-        </h3>
-        <span>
-          <Image
-            src="/gntc.png"
-            width={150}
-            height={150}
-            alt="GNTC"
-            className="rounded-lg md:w-[75px] md:h-[75px] lg:my-4"
-          />
-        </span>
-      </li>
-      <li className="mb-2">
-        <span className="text-lg"> Expected Graduation: </span>
-        <p className="text-orange-100 text-md">Summer 2024</p>
-      </li>
-      <li className="mb-2">
-        <span className="text-lg mb-2">Degree: </span>
-        <p className="text-orange-100 text-md"> Associate of Applied Science</p>
-        <span className="text-lg">Major: </span>
-        <p className="text-orange-100 text-md">Web Site Design/Development</p>
-      </li>
-    </ul>
+    <div className="lg:w-1/4">
+      <ul className="gap-2 md:mb-4 w-full ">
+        <li className="text-2xl mb-2 font-medium text-orange-100">Education</li>
+        <li className="flex flex-col justify-between lg:justify-start lg:items-start gap-2">
+          <h3 className="text-xl mb-2 font-semibold md:mb-0 md:mr-4">
+            Georgia Northwestern <br /> Technical College
+          </h3>
+          <span>
+            <Image
+              src="/gntc.png"
+              width={150}
+              height={150}
+              alt="GNTC"
+              className="rounded-lg md:w-[75px] md:h-[75px] lg:my-4"
+            />
+          </span>
+        </li>
+        <li className="mb-2">
+          <span className="text-lg mb-2">Degree: </span>
+          <p className="text-orange-100 text-md">
+            Associate of Applied Science
+          </p>
+          <span className="text-lg">Major: </span>
+          <p className="text-orange-100 text-md">Website Design/Development</p>
+          <span className="text-lg">Minor: </span>
+          <p className="text-orange-100 text-md">IT Support Specialist</p>
+        </li>
+        <li className="mb-2 ">
+          <span className="text-lg">Relevant Coursework: </span>
+          <p className="text-orange-100 text-md ">
+            Information Security Fundamentals, IT Analysis Design & Project
+            Management, Hardware Installation & Maintenance
+          </p>
+        </li>
+      </ul>
+    </div>
   );
 };
 
