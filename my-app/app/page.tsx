0cb6b03c1667b6603b0d4bd7b80b95983ef79fc1
BigBobandy <jdykes207@gmail.com>
2024-08-02 16:53:03 -0400

    update image dimensions in Home component

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,7 +4,7 @@ import Content from "./components/Content";
 export default function Home() {
   return (
     <main className="flex flex-col md:flex-row justify-center items-center w-full pt-5">
-      <div className="hidden md:block w-[400px] h-[700px] rounded-xl overflow-hidden shadow-md drop-shadow-md">
+      <div className="hidden lg:block w-[400px] h-[700px] rounded-xl overflow-hidden shadow-md drop-shadow-md">
         <Image
           src="/headshot.png"
           width={400}
