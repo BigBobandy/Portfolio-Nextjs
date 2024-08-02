1ebedb10300396d2340e31793191c6b9d5bdd886
BigBobandy <jdykes207@gmail.com>
2024-08-02 17:28:35 -0400

    update fixed issue with unescaped single quotes

--- my-app/app/components/Work.tsx
+++ my-app/app/components/Work.tsx
@@ -6,25 +6,23 @@ const Work = () => {
       <h2 className="text-2xl mb-2 font-medium text-orange-100">
         Work Experience
       </h2>
-      <div className="flex flex-col justify-between gap-8">
-        <p className="text-md font-light text-white ">
-          I am a Freelance Web Developer with experience in designing and
-          developing responsive, user-friendly websites for various clients. My
-          recent work includes the Nail Envy website, where I created a modern
-          and visually appealing site to showcase the salon's services, customer
-          reviews, and more.
-        </p>
-        <p className="text-md font-light text-white ">
-          I specialize in both front-end and back-end development, using
-          technologies like React.js, Tailwind CSS, and Node.js to deliver
-          tailored solutions that meet client needs.
-        </p>
-        <p className="text-md font-light text-white ">
-          While I am actively seeking new clients, I am also pursuing a
-          long-term role as a Full Stack Developer. My ambition is to continue
-          expanding my expertise and contribute to innovative projects.
-        </p>
-      </div>
+      <p className="text-md font-light text-white md:mb-4">
+        I am a Freelance Web Developer with experience in designing and
+        developing responsive, user-friendly websites for various clients. My
+        recent work includes the Nail Envy website, where I created a modern and
+        visually appealing site to showcase the salon&apos;s services, customer
+        reviews, and more.
+      </p>
+      <p className="text-md font-light text-white md:mb-4">
+        I specialize in both front-end and back-end development, using
+        technologies like React.js, Tailwind CSS, and Node.js to deliver
+        tailored solutions that meet client needs.
+      </p>
+      <p className="text-md font-light text-white md:mt-6">
+        While I am actively seeking new clients to collaborate with, I am also
+        pursuing a long-term role as a Full Stack Developer. My ambition is to
+        continue expanding my expertise and contribute to innovative projects.
+      </p>
     </div>
   );
 };
