ec0aa1567d43a4e0b1047369de2e20130c414e87
BigBobandy <jdykes207@gmail.com>
2024-08-02 16:53:43 -0400

    Update Work component content and styles for improved readability and clarity

--- my-app/app/components/Work.tsx
+++ my-app/app/components/Work.tsx
@@ -2,22 +2,29 @@ import React from "react";
 
 const Work = () => {
   return (
-    <div className="flex flex-col  max-w-md lg:w-1/4  bg-opacity-50 rounded-lg ">
+    <div className="flex flex-col max-w-md lg:w-1/4 bg-opacity-50 rounded-lg">
       <h2 className="text-2xl mb-2 font-medium text-orange-100">
         Work Experience
       </h2>
-      <p className="text-md font-light text-white md:mb-4">
-        As a dedicated full-time student expected to graduate in August 2024
-        with a degree in Web Site Design/Development, I am looking for
-        opportunities that allow me to apply and expand my skills in practical
-        settings.
-      </p>
-      <p className="text-md font-light text-white md:mt-6">
-        While I pursue work opportunities, I am also committed to enhancing my
-        skills through personal projects in my spare time. My ambition is to
-        become a leading full-stack developer, recognized for my ability to
-        tackle complex challenges with innovative solutions.
-      </p>
+      <div className="flex flex-col justify-between gap-8">
+        <p className="text-md font-light text-white ">
+          I am a Freelance Web Developer with experience in designing and
+          developing responsive, user-friendly websites for various clients. My
+          recent work includes the Nail Envy website, where I created a modern
+          and visually appealing site to showcase the salon's services, customer
+          reviews, and more.
+        </p>
+        <p className="text-md font-light text-white ">
+          I specialize in both front-end and back-end development, using
+          technologies like React.js, Tailwind CSS, and Node.js to deliver
+          tailored solutions that meet client needs.
+        </p>
+        <p className="text-md font-light text-white ">
+          While I am actively seeking new clients, I am also pursuing a
+          long-term role as a Full Stack Developer. My ambition is to continue
+          expanding my expertise and contribute to innovative projects.
+        </p>
+      </div>
     </div>
   );
 };
